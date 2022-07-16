edda96fa7f518967b118879e9946d474d848d83d
Yakuza <sterplay2010@yahoo.com>
2022-07-16 14:08:52 -0500

    Created View Candidates process

---
+++ src/components/vacants_process_components/MainContent.jsx
@@ -0,0 +1,11 @@
+export const MainContent = () => {
+	return (
+		<div
+			className='h-max'
+			style={{ background: '#EAF3FB' }}>
+			<div className='flex justify-content-center flex-wrap card-container pt-6'>
+                
+            </div>
+		</div>
+	);
+};
