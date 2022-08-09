7793657f3a0939efe11fe79e648de27772667840
Jose Manuel Rodriguez Pavon <20193tn166@utez.edu.mx>
2022-08-09 11:37:59 -0500

    add hot fix select template

--- src/components/cv_selection/DataViewHomeCv.jsx
+++ src/components/cv_selection/DataViewHomeCv.jsx
@@ -1,32 +1,24 @@
 
 import { NavBarApp } from '../navbar/NavBarApp';
 import { CVCardData } from './CvCardData';
-import { PaginatorData } from '../paginator_data/PaginatorData';
 
 
 export const DataViewHomeCv = () => {
 
-  return(
+  return (
     <>
-      <div className='h-max' style={{background:'#EAF3FB'}}>
-        <NavBarApp/>
-
-   
-
+      <div className='h-max' style={{ background: '#EAF3FB' }}>
+        <NavBarApp />
         <div className="flex justify-content-center flex-wrap card-container pl-8 pr-8 pt-6 pb-4">
           <div className="grid container">
-            {[...Array(3).keys()].map(e=>{
-              return(
+            {[...Array(3).keys()].map(e => {
+              return (
                 <div className="col-12 md:col-4 lg:col-4">
-                  <CVCardData/>
+                  <CVCardData />
                 </div>
               )
             })}
           </div>
-        </div>
-
-        <div className="flex justify-content-center flex-wrap card-container pb-6">
-          <PaginatorData/>
         </div>
       </div>
     </>
